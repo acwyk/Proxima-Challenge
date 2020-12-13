8faa1856406bd69739e9d44faa16ce9b8857dc3a
Adam Cwyk <git@adamcwyk.dev>
2020-12-13 13:38:57 +1100

    Add Binance API and WebSocket connections

--- binance.js
+++ binance.js
@@ -1,9 +1,33 @@
+const axios = require('axios');
 const WebSocket = require('ws');
 
-function init_ws_streams(ws){
+function init_ws_streams(config){
+	let ws_url = `${config.uri}:${config.port}/${config.uri_ext}/${config.streams.bnbbtc}@depth`;
+	console.log(ws_url)
+	ws = new WebSocket(ws_url);
+	return ws;
 }
 
+function call_binance_api(config){
+	let api_url = `${config.uri}?symbol=${config.symbols.bnbbtc}&limit=${config.limit}`;
+	console.log(api_url);
+	return new Promise(async (resolve, reject) => {
+		await axios.get(api_url, {
+			method: 'GET'
+		}).then(response  => {
+			console.log("SUCCESS");
+			resolve(response);
+		}).catch((err) => {
+			console.log("ERROR");
+			reject(err);
+		})
+	});
+}
+
+
 module.exports = {
-	init: init,
-	isInit: isInit
+	init_ws_streams: init_ws_streams,
+	call_binance_api: call_binance_api
 };
+
+
--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const config = require('config');
-const websocket = require('ws')
+const WebSocket = require('ws')
 const binance = require('./binance.js');
 
-console.log(config);
+let ws = binance.init_ws_streams(config.binance.ws);
 
-binance.init(config.binance);
+ws.on('open', () => {
+	console.log("WebSocket connected");
+})
 
-if (binance.isInit() == undefined) {
-	console.log("Binance Uninitialized...");
+ws.on('close', () =>{
+	console.log("WebSocket closed");
+})
+
+ws.on('message', (data) => {
+	console.log("message");
+	console.log(data);
+})
+
+ws.on('error', (err) => {
+	console.log(err);
 	process.exit(1);
-}
 
-console.logs(binance);
+})
+
+binance.call_binance_api(config.binance.api).then((data) => {
+	console.log("SUCCESS API");
+	console.log(data);
+}).catch((err) => {
+	console.log("ERROR API");
+	console.log(err);
+});
